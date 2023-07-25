e608c4816f053fa36a911d84df337df5c8acb243
hulkcao <hulkcao@tencent.com>
2023-07-25 08:58:33 +0800

    format code

--- binary_tree/47.cpp
+++ binary_tree/47.cpp
@@ -4,8 +4,9 @@
  @date       2022-04-13 23:40:40
  @reference 剑指offer专项
  @url      https://leetcode-cn.com/problems/pOCWxh/
- @brief  给定一个二叉树 根节点 root ，树的每个节点的值要么是 0，要么是 1。请剪除该二叉树中所有节点的值为 0 的子树。
-节点 node 的子树为 node 本身，以及所有 node 的后代。
+ @brief  给定一个二叉树 根节点 root ，树的每个节点的值要么是 0，要么是
+1。请剪除该二叉树中所有节点的值为 0 的子树。 节点 node 的子树为 node
+本身，以及所有 node 的后代。
 */
 #include <assert.h>
 #include <stdio.h>
@@ -20,93 +21,93 @@ using namespace std;
 
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
-class Solution
-{
-   public:
-    //后序遍历的性质
-    TreeNode *pruneTree(TreeNode *root)
-    {
-        if (!root) return nullptr;
-        root->left = pruneTree(root->left);
-        root->right = pruneTree(root->right);
-        if (!root->left && !root->right && root->val == 0) return nullptr;
-        return root;
-    }
+class Solution {
+public:
+  // 后序遍历的性质
+  TreeNode *pruneTree(TreeNode *root) {
+    if (!root)
+      return nullptr;
+    root->left = pruneTree(root->left);
+    root->right = pruneTree(root->right);
+    if (!root->left && !root->right && root->val == 0)
+      return nullptr;
+    return root;
+  }
 };
 
-class Solution1 : public Solution
-{
-   public:
-    //非递归
-    TreeNode *pruneTree(TreeNode *root)
-    {
-        auto cur = root;
-        stack<TreeNode *> tree_stack;
-        TreeNode *pre = nullptr;
-        while (cur || !tree_stack.empty()) {
-            while (cur) {
-                tree_stack.push(cur);
-                cur = cur->left;
-            }
-            cur = tree_stack.top();
-            if (cur->right && cur->right != pre) {
-                cur = cur->right;
-            } else {
-                tree_stack.pop();
-                if (!cur->left && !cur->right && cur->val == 0) {
-                    if (tree_stack.empty()) return nullptr;
-                    auto parent = tree_stack.top();
-                    if (parent->left == cur) parent->left = nullptr;
-                    if (parent->right == cur) parent->right = nullptr;
-                }
-                pre = cur;
-                cur = nullptr;
-            }
+class Solution1 : public Solution {
+public:
+  // 非递归
+  TreeNode *pruneTree(TreeNode *root) {
+    auto cur = root;
+    stack<TreeNode *> tree_stack;
+    TreeNode *pre = nullptr;
+    while (cur || !tree_stack.empty()) {
+      while (cur) {
+        tree_stack.push(cur);
+        cur = cur->left;
+      }
+      cur = tree_stack.top();
+      if (cur->right && cur->right != pre) {
+        cur = cur->right;
+      } else {
+        tree_stack.pop();
+        if (!cur->left && !cur->right && cur->val == 0) {
+          if (tree_stack.empty())
+            return nullptr;
+          auto parent = tree_stack.top();
+          if (parent->left == cur)
+            parent->left = nullptr;
+          if (parent->right == cur)
+            parent->right = nullptr;
         }
-        return root;
+        pre = cur;
+        cur = nullptr;
+      }
     }
+    return root;
+  }
 };
 
-void show(TreeNode *root)
-{
-    queue<TreeNode *> tree_queue;
-    tree_queue.push(root);
-    while (!tree_queue.empty()) {
-        int size = tree_queue.size();
-        while (size-- > 0) {
-            auto node = tree_queue.front();
-            if (node) {
-                cout << node->val << " ";
-                tree_queue.push(node->left);
-                tree_queue.push(node->right);
-            } else {
-                cout << "#";
-            }
+void show(TreeNode *root) {
+  queue<TreeNode *> tree_queue;
+  tree_queue.push(root);
+  while (!tree_queue.empty()) {
+    int size = tree_queue.size();
+    while (size-- > 0) {
+      auto node = tree_queue.front();
+      if (node) {
+        cout << node->val << " ";
+        tree_queue.push(node->left);
+        tree_queue.push(node->right);
+      } else {
+        cout << "#";
+      }
 
-            tree_queue.pop();
-        }
-        cout << endl;
+      tree_queue.pop();
     }
+    cout << endl;
+  }
 }
 
-int main()
-{
-    TreeNode *root = new TreeNode(1, new TreeNode(0), new TreeNode(1));
-    root->left->left = new TreeNode(0);
-    root->left->right = new TreeNode(0);
-    root->right->left = new TreeNode(0);
-    root->right->right = new TreeNode(1);
+int main() {
+  TreeNode *root = new TreeNode(1, new TreeNode(0), new TreeNode(1));
+  root->left->left = new TreeNode(0);
+  root->left->right = new TreeNode(0);
+  root->right->left = new TreeNode(0);
+  root->right->right = new TreeNode(1);
 
-    Solution1 s;
-    s.pruneTree(root);
-    show(root);
-    return 0;
+  Solution1 s;
+  s.pruneTree(root);
+  show(root);
+  return 0;
 }
--- stack/36.cpp
+++ stack/36.cpp
@@ -9,7 +9,8 @@
 有效的算符包括 +、-、*、/ 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
 说明：
 整数除法只保留整数部分。
-给定逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。
+给定逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0
+的情况。
 
 */
 #include <assert.h>
@@ -22,47 +23,45 @@
 #include <vector>
 
 using namespace std;
-class Solution
-{
-   public:
-    int evalRPN(vector<string>& tokens)
-    {
-        stack<int> stack_store;
-        for (const auto& str : tokens) {
-            if (str == "+" || str == "-" || str == "*" || str == "/") {
-                int num2 = stack_store.top();
-                stack_store.pop();
-                int num1 = stack_store.top();
-                stack_store.pop();
-                switch (str[0]) {
-                case '+':
-                    stack_store.emplace(num1 + num2);
-                    break;
-                case '-':
-                    stack_store.emplace(num1 - num2);
-                    break;
-                case '*':
-                    stack_store.emplace(num1 * num2);
-                    break;
-                case '/':
-                    assert(num2 != 0);
-                    stack_store.emplace(num1 / num2);
-                    break;
-                default:
-                    break;
-                }
-            } else {
-                stack_store.emplace(stoi(str));
-            }
+class Solution {
+public:
+  int evalRPN(vector<string> &tokens) {
+    stack<int> stack_store;
+    for (const auto &str : tokens) {
+      if (str == "+" || str == "-" || str == "*" || str == "/") {
+        int num2 = stack_store.top();
+        stack_store.pop();
+        int num1 = stack_store.top();
+        stack_store.pop();
+        switch (str[0]) {
+        case '+':
+          stack_store.emplace(num1 + num2);
+          break;
+        case '-':
+          stack_store.emplace(num1 - num2);
+          break;
+        case '*':
+          stack_store.emplace(num1 * num2);
+          break;
+        case '/':
+          assert(num2 != 0);
+          stack_store.emplace(num1 / num2);
+          break;
+        default:
+          break;
         }
-        return stack_store.top();
+      } else {
+        stack_store.emplace(stoi(str));
+      }
     }
+    return stack_store.top();
+  }
 };
-int main()
-{
-    vector<string> in{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
-    Solution s;
-    int ret = s.evalRPN(in);
-    cout << ret << endl;
-    return 0;
+int main() {
+  vector<string> in{"10", "6", "9",  "3", "+", "-11", "*",
+                    "/",  "*", "17", "+", "5", "+"};
+  Solution s;
+  int ret = s.evalRPN(in);
+  cout << ret << endl;
+  return 0;
 }
